392c6c4dfbddce97ca8f1d96e8b2e5f80f60e108
Kailas Manikam <kailashraina9@gmail.com>
2024-09-30 17:40:52 +0530

    py files

---
+++ web scarping(python)/Web.py
@@ -0,0 +1,29 @@
+# s = soup.find('div', class_='col-lg-3')
+# content = soup.find( class_ = "wrapper" )
+#
+# print("div ",content)
+# # Parsing the HTML
+class_list = set()
+
+# Find the 'a' tag
+a_tag = soup.find('div')
+
+# Extract the value of the 'href' attribute
+href_value = a_tag['class']
+
+# get all tags
+tags = {tag.name for tag in soup.find_all()}
+
+# iterate all tags
+for tag in tags:
+
+    # find all element of tag
+    for i in soup.find_all(tag):
+
+        # if tag has attribute of class
+        if i.has_attr("class"):
+
+            if len(i['class']) != 0:
+                class_list.add(" ".join(i['class']))
+
+print(class_list)
